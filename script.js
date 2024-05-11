c628250eb8c602442c70907f43cda71bbc7e5f32
1medi <144403286+1medi@users.noreply.github.com>
2024-05-10 18:47:50 -0700

    stuffs

--- script.js
+++ script.js
@@ -11,5 +11,13 @@ function updateHeader(value){
   document.getElementById("username-header").textContent = value;
 }
 
+var myImages = [["https://img.olympics.com/images/image/private/t_s_pog_staticContent_hero_xl_2x/f_auto/primary/c5r52rbifxn2srhp9no0"], ["https://draxe.com/wp-content/uploads/2015/01/BananaNutritionFB.jpg"]];
+var myIndex = 0;
+var print = document.getElementById('option');
 
+function changeBackground() {
+  var selectedIndex = document.querySelector("select").value;
+  document.body.style.backgroundImage = "url('" + myImages[selectedIndex] + "')";
+  console.log(myImages)
+}
 
