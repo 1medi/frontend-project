3db657b8f94fc6752545cd163819e06cc61ffaa1
Deluka Loku Gam Hewage <dlokugamhewage@se14l0120-08.ad.bcit.ca>
2024-05-24 10:45:10 -0700

    upading0

--- script.js
+++ script.js
@@ -27,16 +27,16 @@ function changeBackground() {
 
 
 
-setInterval(setClock, 1000); //Run every 1000 milliseconds = 1 second
+setInterval(setClock, 1000); 
 
 const hourHand = document.querySelector("[data-hour-hand]");
 const minuteHand = document.querySelector("[data-minute-hand]");
 const secondHand = document.querySelector("[data-second-hand]");
 
 function setClock() {
-  var currentDate = new Date(); //Gets current date and time
+  var currentDate = new Date(); 
   var secondsRatio = currentDate.getSeconds() / 60;
-  // Move minute and hour hands gradually according to seconds and minutes
+  
   var minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
   var hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
   setRotation(secondHand, secondsRatio);
@@ -45,7 +45,7 @@ function setClock() {
 }
 
 function setRotation(element, rotationRatio) {
-  // Sets property of element to rotation variable in CSS
+
   element.style.setProperty("--rotation", rotationRatio * 360);
 }
 
@@ -62,15 +62,11 @@ function displayDigitalTime() {
     session.innerHTML = "AM";
   }
 
-  // Changes format from 24 hour clock to 12 hour clock
   if (hrs > 12) {
     hrs = hrs - 12;
   }
 
-  // Because minutes and seconds were not getting a padded 0 if the number
-  // was less than 10, I have to use an extra if statement for minutes and
-  // seconds. I found my answer here:
-  // https://www.tutorialspoint.com/How-to-pad-a-number-with-leading-zeros-in-JavaScript
+
 
   if (min < 10) {
     let paddedMin = min.toString().padStart(2, "0");
@@ -87,19 +83,19 @@ function displayDigitalTime() {
   document.getElementById("seconds").innerHTML = sec;
 }
 
-//https://stackoverflow.com/questions/10687479/javascript-replace-div-on-each-click
+
 function toggleClock() {
   var analog = document.getElementById("analog-clock");
   var digital = document.getElementById("digital-clock");
-  // If analog clock is not displayed, display it in place of digital clock.
+
   if (analog.style.display === "none") {
     analog.style.display = "block";
     digital.style.display = "none";
-    //https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+
     document.getElementById("btn").innerHTML =
       "Digital Clock";
   } else {
-    // If digital clock is not displayed, display it in place of analog clock.
+   
     analog.style.display = "none";
     digital.style.display = "block";
     setInterval(displayDigitalTime, 10);
